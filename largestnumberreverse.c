f4553241142d34f2493c7315ac0c869b377af7a5
yuvaraniyuva <40492725+yuvaraniyuva@users.noreply.github.com>
2019-02-04 00:31:33 -0800

    Create largestnumberreverse.c

---
+++ largestnumberreverse.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+    void main ()
+    {
+ 
+        int number[30];
+ 
+        int i, j, a, no;
+       
+        scanf("%d", &n);
+ 
+       
+        for (i = 0; i < no; ++i)
+	        scanf("%d", &number[i]);
+ 
+        /*  sorting begins ... */
+ 
+        for (i = 0; i < no; ++i) 
+        {
+            for (j = i + 1; j < no; ++j) 
+            {
+                if (number[i] < number[j]) 
+                {
+                    a = number[i];
+                    number[i] = number[j];
+                    number[j] = a;
+                }
+            }
+        }
+ 
+ 
+        for (i = 0; i < n0; ++i) 
+        {
+            printf("%d", number[i]);
+        }
+ 
+    }
