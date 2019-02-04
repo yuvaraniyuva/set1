0746779ee12077c961e2a8668a6ed7550fedb210
yuvaraniyuva <40492725+yuvaraniyuva@users.noreply.github.com>
2019-02-04 00:42:27 -0800

    Update largestnumberreversed.c

--- largestnumberreversed.c
+++ largestnumberreversed.c
@@ -1,34 +1,24 @@
 #include <stdio.h>
     void main ()
     {
- 
-        int number[30];
- 
-        int i, j, a, no;
-       
-        scanf("%d", &n);
- 
-       
-        for (i = 0; i < no; ++i)
-	        scanf("%d", &number[i]);
- 
-        /*  sorting begins ... */
- 
-        for (i = 0; i < no; ++i) 
+int number[30];
+int i, j, a, no;
+scanf("%d", &n);
+for (i = 0; i < no; ++i)
+scanf("%d", &number[i]);
+ for (i = 0; i < no; ++i) 
+{
+  for (j = i + 1; j < no; ++j) 
         {
-            for (j = i + 1; j < no; ++j) 
+         if (number[i] < number[j]) 
             {
-                if (number[i] < number[j]) 
-                {
-                    a = number[i];
-                    number[i] = number[j];
-                    number[j] = a;
+               a = number[i];
+               number[i] = number[j];
+               number[j] = a;
                 }
             }
         }
- 
- 
-        for (i = 0; i < n0; ++i) 
+ for (i = 0; i < n0; ++i) 
         {
             printf("%d", number[i]);
         }
