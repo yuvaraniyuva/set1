be6225ca9f718f92c7879d9f05fcaec225480b4f
yuvaraniyuva <40492725+yuvaraniyuva@users.noreply.github.com>
2019-02-04 18:44:11 +0530

    Update uniquenumber.cpp

--- uniquenumber.cpp
+++ uniquenumber.cpp
@@ -1,20 +1,20 @@
 #include<stdio.h>
 int main(){
-int n,i,j,a[1000],occur[100];
-scanf("%d",&n);
-for(i=0;i<n;i++){
+int no,i,j,a[1000],occur[100];
+scanf("%d",&no);
+for(i=0;i<no;i++){
 	scanf("%d",&a[i]);
 	occur[i]=0;
 }
-for(i=0;i<n;i++)
-for(j=0;j<n;j++)
+for(i=0;i<no;i++)
+for(j=0;j<no;j++)
 {
 	if(a[i]==a[j])
 	{
 		occur[i]++;
 	}
 }
-for(i=0;i<n;i++)
+for(i=0;i<no;i++)
 {
 	if(occur[i]==1)
 	printf(" %d",a[i]);
